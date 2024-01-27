afa272c6dba230cc7be33e3f0948f62e6145cd1c
krmn7 <krmnlucas7@gmail.com>
2024-01-27 16:44:59 +0100

    4 - add link appear animation

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const shadowContainer = document.querySelector('.navbar-shadow');
-
+    const navLinks = document.querySelectorAll('.nav-links li')
+    
     burger.addEventListener('click', () =>{
         nav.classList.toggle('nav-active');
         shadowContainer.classList.toggle('navbar-shadow-active');
         burger.classList.toggle('burger-active');
+
+        navLinks.forEach((link, index) =>{
+            if (link.style.animation){
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/4 + 0.5}s`
+            }
+        })
     })
 
 
